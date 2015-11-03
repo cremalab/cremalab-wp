512968cc8f7bb137287b9fc781ee0b0472703778
IMAHiji <lukeaskins@gmail.com>
2015-11-03 09:27:40 -0600

    Merge Commit

---
+++ src/cremalab/header.php
@@ -0,0 +1,14 @@
+<div class="Cremalab__Navbar">
+
+  <div class="Cremalab__Navbar-logoImageContainer">
+
+    <img class=Cremalab__Navbar-logoImage src="./wp-content/themes/cremalab/assets/images/cremalab_logo.svg" alt="" /> <span class="Cremalab_Nabar-logoTitle">Crema</span>
+  </div>
+
+  <div class="Cremalab__Nav-navItems">
+    <ul>
+      <?php wp_list_pages('title_li='); ?>
+    </ul>
+  </div>
+
+</div>
