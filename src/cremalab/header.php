9b4d428834f21d1d8c1b3458cb1ba82c3f640a28
IMAHiji <lukeaskins@gmail.com>
2015-11-06 10:46:51 -0600

    fix heirarchy for blog, hook up posts to blog page

--- src/cremalab/header.php
+++ src/cremalab/header.php
@@ -1,9 +1,7 @@
 <div class="Cremalab__Navbar">
 
-  <a href="#" class="Cremalab__Navbar-logoImageContainer">
-
+  <a href="/" class="Cremalab__Navbar-logoImageContainer">
       <img class="Cremalab__Navbar-logoImage" src="../wp-content/themes/cremalab/assets/images/cremalab_logo.svg" alt="" /> <span class="Cremalab__Nabar-logoTitle">Crema</span>
-
   </a>
 
   <div class="Cremalab__Nav-navItems">
