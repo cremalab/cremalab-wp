03906838fc683e81360e7476afce041b729a370a
IMAHiji <lukeaskins@gmail.com>
2015-11-06 14:28:30 -0600

    working on the home page...freakin' logos

--- src/cremalab/front-page.php
+++ src/cremalab/front-page.php
@@ -3,7 +3,8 @@
 
 <?php get_header(); ?>
 
-  <div class="Cremalab__Homepage--videoBackgroundWrapper">
+
+<div class="Cremalab__Homepage--videoBackgroundWrapper">
     <video autobuffer="autobuffer" autoplay="autoplay" class="Cremalab__Homepage-videoBackground" loop="loop">
       <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.mp4" type="video/mp4; codecs=avc1.42E01E,mp4a.40.2" />
       <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.webm" type="video/webm; codecs=vp8,vorbis" />
@@ -11,10 +12,17 @@
     </video>
 
     <div class="Cremalab__Homepage-titleWrapper">
-        <img src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg" class="Cremalab__Homepage--TitleLogo" alt="CREMA LOGO">
+      <img src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg" class="Cremalab__Homepage--TitleLogo" alt="CREMA LOGO">
     </div>
-  </div>
-<h1>WORK STUFF GOES HERE</h1>
+
+</div>
+
+
+
+
+
+
+
 </div>
 <!-- footer if any -->
 <?php get_footer(); ?>
--- src/cremalab/header.php
+++ src/cremalab/header.php
@@ -1,7 +1,8 @@
 <div class="Cremalab__Navbar">
 
   <a href="/" class="Cremalab__Navbar-logoImageContainer">
-      <img class="Cremalab__Navbar-logoImage" src="../wp-content/themes/cremalab/assets/images/cremalab_logo.svg" alt="" /> <span class="Cremalab__Nabar-logoTitle">Crema</span>
+      <img class="Cremalab__Navbar-logoImage" src="../wp-content/themes/cremalab/assets/images/cremalab_logo.svg" alt="" />
+      <span class="Cremalab__Nabar-logoTitle">Crema</span>
   </a>
 
   <div class="Cremalab__Nav-navItems">
