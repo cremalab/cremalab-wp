0fadbb7d6c51bc7c6f0dee426cb3e29ba4909a7d
IMAHiji <lukeaskins@gmail.com>
2015-11-11 11:54:07 -0600

    Simplify front-page markup

--- src/cremalab/front-page.php
+++ src/cremalab/front-page.php
@@ -4,18 +4,20 @@
 <?php get_header(); ?>
 
 
-<div class="Cremalab__Homepage--videoBackgroundWrapper">
+
     <video autobuffer="autobuffer" autoplay="autoplay" class="Cremalab__Homepage-videoBackground" loop="loop">
       <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.mp4" type="video/mp4; codecs=avc1.42E01E,mp4a.40.2" />
       <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.webm" type="video/webm; codecs=vp8,vorbis" />
       <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.ogv" type="video/ogg; codecs=theora,vorbis" />
     </video>
 
-    <div class="Cremalab__Homepage-titleWrapper">
-      <img src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg" class="Cremalab__Homepage--TitleLogo" alt="CREMA LOGO">
-    </div>
 
-</div>
+
+
+
+
+
+<img src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg" class="Cremalab__Homepage--TitleLogo" alt="CREMA LOGO">
 
 
 
--- src/cremalab/templates/team-list.php
+++ src/cremalab/templates/team-list.php
@@ -5,11 +5,10 @@
 
   <div class="Cremalab__teamMemberWrapper">
         <img src="<?php echo $image['url'] ?>" alt="" class="Cremalab__teamMemberPhoto">
-        <h3 class="Cremalab__teamMemberName"><?php the_field('employee_name') ?></h3>
-        <p class="Cremalab__teamMemberTitle"><?php the_field('title') ?></p>
 
 
-    <div class="Cremalab__teamMemberInfo">
+
+    <div class="Cremalab__teamMemberInfo animated once slideInUp">
       <p class="Cremalab__teamQuestionsCopy">Favorite Movie: <span class="Cremalab__teamAnswersCopy"><?php the_field('favorite_movie') ?></span></p>
       <p class="Cremalab__teamQuestionsCopy">Favorite thing about kansas: <span class="Cremalab__teamAnswersCopy"><?php the_field('favorite_kc_thing') ?></span></p>
       <p class="Cremalab__teamQuestionsCopy">City: <span class="Cremalab__teamAnswersCopy">CITY</span></p>
@@ -18,6 +17,10 @@
       <p class="Cremalab__teamQuestionsCopy">WHat you wanted to be when you grew up: <span class="Cremalab__teamAnswersCopy"><?php the_field('be_when_grow_up') ?></span></p>
       <p class="Cremalab__teamQuestionsCopy">What would your super power be: <span class="Cremalab__teamAnswersCopy"><?php the_field('super_power') ?></span></p>
       <p class="Cremalab__teamQuestionsCopy">Also involved in: <span class="Cremalab__teamAnswersCopy"><?php the_field('also_involved_in') ?></span></p>
+    </div>
+    <div class="Cremalab__teamMemberNameTitle">
+      <h3 class="Cremalab__teamMemberName"><?php the_field('employee_name') ?></h3>
+      <p class="Cremalab__teamMemberTitle"><?php the_field('title') ?></p>
     </div>
 
 
