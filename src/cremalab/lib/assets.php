52268db4513a89cb51c9ece3555d45341f552a5d
IMAHiji <lukeaskins@gmail.com>
2015-11-13 11:50:08 -0600

    add velocity, do animation on info hover

--- src/cremalab/lib/assets.php
+++ src/cremalab/lib/assets.php
@@ -75,6 +75,8 @@ function assets() {
 
   wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
   wp_enqueue_script('sage_js', asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('velocity', asset_path('scripts/velocity.js'), ['jquery'], null, true);
+  wp_enqueue_script('custom', asset_path('scripts/custom.js'), ['jquery'], null, true);
 
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
