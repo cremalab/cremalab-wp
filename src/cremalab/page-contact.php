8ba18991ff4dce34db58a7eeb46340e1941e520a
IMAHiji <lukeaskins@gmail.com>
2015-11-16 09:27:23 -0600

    start styling contact page

--- src/cremalab/page-contact.php
+++ src/cremalab/page-contact.php
@@ -2,7 +2,22 @@
   <?php get_header(); ?>
 </div>
 
-<h1>This is the Contact Page</h1>
+<div class="Cremalab__ContactPageWrapper">
+  <h1>Work with us</h1>
+  <p>Want to work with us?  Fill out <span>this form</span></p>
+
+  <h1>Join Us</h1>
+  <p>We're hiring.  <span>Learn more</span></p>
+
+  <h1>Follow Us</h1>
+  <ul class="Cremalab__ContactPage-contactlist" >
+    <li><span class="contact-icons icon-facebook-circle"></span></li>
+    <li><span class="contact-icons icon-twitter-circle"></span></li>
+    <li><span class="contact-icons icon-instagram"></span></li>
+    <li><span class="contact-icons icon-linked_in-circle"></span></li>
+    <li><span class="contact-icons icon-dribbble-circle"></span></li>
+    <li><span class="contact-icons icon-github-circle"></span></li></ul>
+</div>
 
 
 <?php get_footer(); ?>
