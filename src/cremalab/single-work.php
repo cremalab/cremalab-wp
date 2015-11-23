82c771545a354c25178b5c0fb5e9f69faaddf1cf
IMAHiji <lukeaskins@gmail.com>
2015-11-23 12:11:36 -0600

    case study page styling, add gallery slider

--- src/cremalab/single-work.php
+++ src/cremalab/single-work.php
@@ -1,3 +1,56 @@
-<h1>single-work.php</h1>
+
+<?php
+    $headerImage = get_field('case_study_header_image');
+    $Title = get_field('cs_title');
+    $Subheading = get_field('cs_subheading');
+    $link = get_field('cs_link');
+    $slider = get_field('cs_image_slider');
+
+    $FullWidth1 = get_field('cs_full_width_image_1');
+    $FullWidth2 = get_field('cs_full_width_image_2');
+    $FullWidth3 = get_field('cs_full_width_image_3');
+
+    $BodyText1 = get_field('cs_body_text_1');
+    $BodyText2 = get_field('cs_body_text_2');
+    $BodyText3 = get_field('cs_body_text_3');
+    $BodyText4 = get_field('cs_body_text_4');
 
 
+
+?>
+
+<div class="Cremalab__Navbar-BlackHeader">
+    <?php get_header(); ?>
+</div>
+<div class="Cremalab__CaseStudyWrapper">
+<div class="Cremalab__CaseStudySlider"><?php echo do_shortcode($slider); ?></div>
+<div class="Cremalab__CaseStudyContentContainer">
+    <h1 class="Cremalab__caseStudyTitle">
+        <?php echo $Title ?>
+    </h1>
+
+    <h3 class="Cremalab__caseStudySubHeader"><?php echo $Subheading ?></h3>
+
+    <div class="Cremalab__caseStudyhighlightLine"></div>
+
+    <div class="Cremalab__caseStudyTextContainer"><?php echo $BodyText1 ?></div>
+    <img class="Cremalab__caseStudy--fullWidthImage" src="<?php echo $FullWidth1['url'] ?>" alt="">
+    <div class="Cremalab__caseStudyTextContainer"><?php echo $BodyText2 ?></div>
+    <img class="Cremalab__caseStudy--fullWidthImage" src="<?php echo $FullWidth2['url'] ?>" alt="">
+    <div class="Cremalab__caseStudyTextContainer"><?php echo $BodyText3 ?></div>
+    <img class="Cremalab__caseStudy--fullWidthImage" src="<?php echo $FullWidth3['url'] ?>" alt="">
+    <div class="Cremalab__caseStudyTextContainer">
+        <?php echo $BodyText4 ?>
+        <a class="Cremalab__caseStudyLink" href="<?php echo $link?>">See it live</a>
+    </div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+<?php get_footer(); ?>
