226f5f4004189c2bd0e71cb2b9e84f07b513504b
IMAHiji <lukeaskins@gmail.com>
2015-11-24 16:03:00 -0600

    add work list to front page

--- src/cremalab/front-page.php
+++ src/cremalab/front-page.php
@@ -11,17 +11,18 @@
 <!--      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.ogv" type="video/ogg; codecs=theora,vorbis" />-->
 <!--    </video>-->
 
-
-
 <!--                <img  class="Cremalab__Homepage--heroImage" src="--><?php //echo $heroImage['url']; ?><!-- " alt="">-->
 
     <img class="Cremalab__Homepage--TitleLogo" src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg"  alt="CREMA LOGO">
-
-
-
-
-
-
 </div>
+
+<h1 class="Cremalab__WorkHeader--home">Check out our work</h1>
+
+<?php
+    $args = array( 'post_type' => 'work', 'posts_per_page' => 10 );
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post();
+        get_template_part('templates/work', 'list');
+    endwhile; ?>
 <!-- footer if any -->
 <?php get_footer(); ?>
