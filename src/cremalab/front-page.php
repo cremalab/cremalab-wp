0798c25e76b8157d92f5325c7bb9f7ff3c7d0f03
IMAHiji <lukeaskins@gmail.com>
2015-11-24 14:07:22 -0600

    Change front page to image for now.

--- src/cremalab/front-page.php
+++ src/cremalab/front-page.php
@@ -3,22 +3,19 @@
 
 <?php get_header(); ?>
 
-
-
-    <video autobuffer="autobuffer" autoplay="autoplay" class="Cremalab__Homepage-videoBackground" loop="loop">
-      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.mp4" type="video/mp4; codecs=avc1.42E01E,mp4a.40.2" />
-      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.webm" type="video/webm; codecs=vp8,vorbis" />
-      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.ogv" type="video/ogg; codecs=theora,vorbis" />
-    </video>
-
-
-
+<?php $heroImage = get_field('home_page_hero') ?>
+<!---->
+<!--    <video autobuffer="autobuffer" autoplay="autoplay" class="Cremalab__Homepage-videoBackground" loop="loop">-->
+<!--      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.mp4" type="video/mp4; codecs=avc1.42E01E,mp4a.40.2" />-->
+<!--      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.webm" type="video/webm; codecs=vp8,vorbis" />-->
+<!--      <source src="https://s3.amazonaws.com/cremalab/static/video/cremalab.ogv" type="video/ogg; codecs=theora,vorbis" />-->
+<!--    </video>-->
 
 
 
+<!--                <img  class="Cremalab__Homepage--heroImage" src="--><?php //echo $heroImage['url']; ?><!-- " alt="">-->
 
-<img src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg" class="Cremalab__Homepage--TitleLogo" alt="CREMA LOGO">
-
+    <img class="Cremalab__Homepage--TitleLogo" src="../wp-content/themes/cremalab/assets/images/Crema_WordLogoWhite.svg"  alt="CREMA LOGO">
 
 
 
