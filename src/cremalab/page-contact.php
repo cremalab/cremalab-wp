f16dd830972036b83ceb849a39fd4284165cd245
IMAHiji <lukeaskins@gmail.com>
2015-11-24 09:53:31 -0600

    fix margin issue with category display on blogroll

--- src/cremalab/page-contact.php
+++ src/cremalab/page-contact.php
@@ -4,24 +4,28 @@
 
 <div class="Cremalab__ContactPageWrapper">
   <h1 class="Cremalab__pageHeaderOne">Work with us</h1>
-  <p class="Cremalab__linkCopy">Want to work with us?  Fill out <span class="Cremalab__ContactPage--underline">this form.</span></p>
-  <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 5 ); } ?>
+
+  <div class="Cremalab__ContactPage-FormWrapper">
+    <p class="Cremalab__linkCopy">Want to work with us?  Fill out <span class="Cremalab__ContactPage--underline">this form.</span></p>
+    <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 5 ); } ?>
+  </div>
+
 
   <div class="Cremalab__ContactPage--spacer"></div>
 
   <h1 class="Cremalab__pageHeaderOne">Join Us</h1>
-  <p class="Cremalab__linkCopy">We're hiring.  <span class="Cremalab__ContactPage--underline">Learn more.</span></p>
+  <p class="Cremalab__linkCopy">We're hiring.  <span class="Cremalab__ContactPage--underline"><a href="/careers">Learn more.</a></span></p>
 
   <div class="Cremalab__ContactPage--spacer"></div>
 
   <h1 class="Cremalab__pageHeaderOne">Follow Us</h1>
   <ul class="Cremalab__ContactPage-contactlist" >
-    <li><span class="contact-icons icon-facebook-circle"></span></li>
-    <li><span class="contact-icons icon-twitter-circle"></span></li>
-    <li><span class="contact-icons icon-instagram"></span></li>
-    <li><span class="contact-icons icon-linked_in-circle"></span></li>
-    <li><span class="contact-icons icon-dribbble-circle"></span></li>
-    <li><span class="contact-icons icon-github-circle"></span></li>
+    <li><a href="#" class="Cremalab__ContactPage--socialLink"><span class="contact-icons icon-facebook-circle"></span></a></li>
+    <li><a href="#" class="Cremalab__ContactPage--socialLink"><span class="contact-icons icon-twitter-circle"></span></a></li>
+    <li><a href="#" class="Cremalab__ContactPage--socialLink"><span class="contact-icons icon-instagram"></span></a></li>
+    <li><a href="#" class="Cremalab__ContactPage--socialLink"><span class="contact-icons icon-linked_in-circle"></span></a></li>
+    <li><a href="#" class="Cremalab__ContactPage--socialLink"><span class="contact-icons icon-dribbble-circle"></span></a></li>
+    <li><a href="#" class="Cremalab__ContactPage--socialLink"><span class="contact-icons icon-github-circle"></span></a></li>
   </ul>
 </div>
 
