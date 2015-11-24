d067b22f65520919929b9064ca2d67076c35f419
IMAHiji <lukeaskins@gmail.com>
2015-11-24 14:35:39 -0600

    work list header images to 100/100

--- src/cremalab/templates/work-list.php
+++ src/cremalab/templates/work-list.php
@@ -5,6 +5,8 @@
     <img src=" <?php echo $headerImage['url'] ?>" alt="" class="Cremalab__Work-HeaderImage">
     <h3 class="Cremalab__WorkSubheader"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
   </div>
+
+
   <div class="Cremalab__Work-HoverContainer">
     <div class="Cremalab__Work-HoverContainer--contentContainer">
       <h3><a class="Cremalab__WorkSubheader--Hover" href="<?php the_permalink()?>"><?php the_title() ?></a></h3>
