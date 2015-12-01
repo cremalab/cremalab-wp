b642c5c9564aee734c1c6a4d14f739038726317f
IMAHiji <lukeaskins@gmail.com>
2015-12-01 13:53:26 -0600

    add white color to mobile x when open on non-homepages

--- src/cremalab/header.php
+++ src/cremalab/header.php
@@ -27,7 +27,7 @@
 <!--$pageID =='180' or $pageID == '24' or $pageID == '27' or $pageID == '138' or $pageID == '20')-->
 
 <?php if($pageID != '2'): ?>
-  <button class="c-hamburger--black c-hamburger--htx">
+  <button class="c-hamburger c-hamburger--black c-hamburger--htx">
     <span class="Cremalab__Navbar--hamburger">toggle menu</span>
   </button>
   <span class="Cremalab__NavbarMoreLabel--black">More</span>
