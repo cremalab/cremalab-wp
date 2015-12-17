3cf4ca7c366481e29a12198276098abf7836aba0
IMAHiji <lukeaskins@gmail.com>
2015-12-17 12:08:21 -0600

    center content in footer along baseline

--- src/cremalab/footer.php
+++ src/cremalab/footer.php
@@ -1,9 +1,10 @@
 <div class="Cremalab__footerWrapper">
 
 <div class="Cremalab__footerWelcomeContainer">
-<h1 class="Cremalab__footer--header">Work with us</h1>
+<div class="Cremalab__footerWelcomeContainerFlex">
+  <h1 class="Cremalab__footer--header">Work with us</h1>
   <p class="Cremalab__footer--text">Want to work with us?  Fill out <span class="Cremalab__footerUnderline">this form</span></p>
-
+</div>
 </div>
 
   <div class="Cremalab__footerFormContainer">
